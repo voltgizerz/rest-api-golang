18d68b37352ec8113d23022406de413fa34ed09a
voltgizerz <felix.asui@gmail.com>
2021-07-24 19:19:15 +0700

    setup env file

--- config/database.go
+++ config/database.go
@@ -2,19 +2,34 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func InitDB() (bool) {
-	dsn := "root:@tcp(localhost)/pokemon?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := GetHost()
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 			log.Println(err)
 	}
 	DB = db
 	return true
+}
+
+func GetHost() (string) {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	host, _ := os.LookupEnv("DB_HOST_LOCAL")
+
+	env, _ := os.LookupEnv("ENV")
+	if env == "production" {
+		host, _ = os.LookupEnv("DB_HOST_PRODUCTION")
+	}
+	return host
 }
