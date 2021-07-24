6e25dc94171e12c20525e6addfff4c7d2f8d57f5
voltgizerz <felix.asui@gmail.com>
2021-07-24 15:46:27 +0700

    fix linter

--- models/pokemon.go
+++ models/pokemon.go
@@ -1,7 +1,7 @@
 package models
 
 type Pokemon struct {
-	Id    							int 			`gorm:"column:pok_id",json:"id"`
+	Id    							int 			`gorm:"column:pok_id" json:"id"`
 	Name  							string		`json:"name"`
 	Height 							int				`json:"height"`
 	Weight 							int		 		`json:"weight"`
